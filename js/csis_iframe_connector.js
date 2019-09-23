2ac7564ca85a928e607690dbec0cc3ac7528a3e0
p-a-s-c-a-l <pascal.dihe@cismet.de>
2019-09-23 15:01:59 +0200

    # 14 implement initTableComponent

--- js/csis_iframe_connector.js
+++ js/csis_iframe_connector.js
@@ -11,94 +11,117 @@
 
     }*/
 
+
+    if (!drupalSettings.csisHelpers) {
+        drupalSettings.csisHelpers = {};
+    }
+
     /**
-     * Update iFrame src attribute.
+     * Extract the parameters from CSISHelpers object and append to the iFrame scr URL
+     * 
+     * @param {String} iFrameUrl 
+     * @param {String} appType 
+     */
+    const addQueryParameters = function (iFrameUrl, appType) {
+        if (undefined !== drupalSettings && undefined !== drupalSettings.csisHelpers) {
+            var csisHelpers = drupalSettings.csisHelpers;
+            var study_uuid, study_area, emikat_id, datapackage_uuid,
+                write_permissions, resource_uuid, minx, miny, maxx, maxy, study_variant, time_period, emission_scenario, event_frequency;
+
+            if (undefined !== csisHelpers.resourceInfo && appType == 'ResourcePreviewMap') {
+                console.info(`showing ${appType} for resource ${csisHelpers.resourceInfo.name}`);
+                resource_uuid = csisHelpers.resourceInfo.uuid;
+                // Yeah, 'study_area' is not correct, but we reuse this query param since we don't want to re-implement 
+                // handling of initial bbox just because the data model contains rubbish. :-/
+                // See https://github.com/clarity-h2020/map-component/issues/53
+                study_area = csisHelpers.resourceInfo.spatial_extent;
+                //minx = csisHelpers.resourceInfo.minx;
+                //miny = csisHelpers.resourceInfo.miny;
+                //maxx = csisHelpers.resourceInfo.maxx;
+                //maxy = csisHelpers.resourceInfo.maxy;
+                write_permissions = csisHelpers.resourceInfo.write_permissions;
+            }
+            else if (undefined !== csisHelpers.datapackageInfo && appType == 'DataPackagePreviewMap') {
+                console.info(`showing ${appType} for datapackage ${csisHelpers.datapackageInfo.name}`);
+                datapackage_uuid = csisHelpers.datapackageInfo.uuid;
+                study_area = csisHelpers.datapackageInfo.spatial_extent;
+                write_permissions = csisHelpers.datapackageInfo.write_permissions;
+            } else if (undefined !== csisHelpers.studyInfo) { // implicitly use for study preview map
+                console.info(`showing ${appType} for study ${csisHelpers.studyInfo.name}`);
+                study_uuid = csisHelpers.studyInfo.uuid;
+                study_area = csisHelpers.studyInfo.study_area;
+                emikat_id = csisHelpers.studyInfo.study_emikat_id;
+                datapackage_uuid = csisHelpers.studyInfo.study_datapackage_uuid;
+                write_permissions = csisHelpers.studyInfo.write_permissions;
+                study_variant = csisHelpers.studyInfo.study_variant;
+                time_period = csisHelpers.studyInfo.time_period;
+                emission_scenario = csisHelpers.studyInfo.emission_scenario;
+                event_frequency = csisHelpers.studyInfo.event_frequency;
+
+            } else if (undefined !== csisHelpers.entityinfo) { // DEPRECATED!
+                console.warn(`showing ${appType} for study ${csisHelpers.entityinfo.study_uuid} for **deprecated** drupalSettings.csisHelpers.entityinfo`);
+                study_uuid = csisHelpers.entityinfo.study_uuid;
+                study_area = csisHelpers.entityinfo.study_area;
+                emikat_id = csisHelpers.entityinfo.study_emikat_id;
+                datapackage_uuid = csisHelpers.entityinfo.study_datapackage_uuid;
+                write_permissions = csisHelpers.entityinfo.write_permissions;
+            } else {
+                console.error(`no entityinfo objects found iFrame ${appType} embedded for unsupported entity type!`);
+            }
+
+            iFrameUrl += study_uuid ? `&study_uuid=${study_uuid}` : '';
+            iFrameUrl += study_area ? `&study_area=${study_area}` : '';
+            iFrameUrl += emikat_id ? `&emikat_id=${emikat_id}` : '';
+            iFrameUrl += datapackage_uuid ? `&datapackage_uuid=${datapackage_uuid}` : '';
+            iFrameUrl += write_permissions ? `&write_permissions=${write_permissions}` : '';
+            iFrameUrl += resource_uuid ? `&resource_uuid=${resource_uuid}` : '';
+            iFrameUrl += minx ? `&minx=${minx}` : '';
+            iFrameUrl += miny ? `&miny=${miny}` : '';
+            iFrameUrl += maxx ? `&maxx=${maxx}` : '';
+            iFrameUrl += maxy ? `&maxy=${maxy}` : '';
+            iFrameUrl += study_variant ? `&maxy=${study_variant}` : '';
+            iFrameUrl += time_period ? `&maxy=${time_period}` : '';
+            iFrameUrl += emission_scenario ? `&maxy=${emission_scenario}` : '';
+            iFrameUrl += event_frequency ? `&maxy=${event_frequency}` : '';
+        } else {
+            console.error('no global csisHelpers object found, probably not connected to Drupal!')
+        }
+
+        return iFrameUrl;
+    }
+
+    /**
+     * Update iFrame src attribute for Map Component Apps.
      * 
      * @param {String} mapType 
      * @param {String} grouping_tag 
      * @param {Object} iFrameMapComponent 
      */
-
-    if(!drupalSettings.csisHelpers) {
-        drupalSettings.csisHelpers = {};
-    }
-
     drupalSettings.csisHelpers.initMapComponent = function initMapComponent(mapType = 'GenericMap', grouping_tag = 'taxonomy_term--eu_gl', iFrameMapComponent = document.getElementById('map-component')) {
         try {
-            
-            if(undefined == iFrameMapComponent || null == iFrameMapComponent) {
+            if (undefined == iFrameMapComponent || null == iFrameMapComponent) {
                 console.warn('initMapComponent(): no iFrameMapComponent available');
                 return;
             }
-            
+
             // window.location.origin instead of window.location.host: we need the protocol, too!
-            var host = window.location.origin, study_uuid, study_area, emikat_id, datapackage_uuid, write_permissions, resource_uuid, minx, miny, maxx, maxy;
+            var host = window.location.origin;
 
-            if (undefined !== drupalSettings && undefined !== drupalSettings.csisHelpers) {
-                var csisHelpers = drupalSettings.csisHelpers;
+            /**
+             * Base map component URL
+             * 
+             * @type {String}
+             */
+            var mapComponentUrl = `${host}/apps/map-component/build/${mapType}/?host=${host}`;
+            mapComponentUrl = addQueryParameters(mapComponentUrl, mapType);
 
-                if (undefined !== csisHelpers.resourceInfo && mapType == 'ResourcePreviewMap') {
-                    console.info(`showing ${mapType} for resource ${csisHelpers.resourceInfo.name}`);
-                    resource_uuid = csisHelpers.resourceInfo.uuid;
-                    // Yeah, 'study_area' is not correct, but we reuse this query param since we don't want to re-implement 
-                    // handling of initial bbox just because the data model contains rubbish. :-/
-                    // See https://github.com/clarity-h2020/map-component/issues/53
-                    study_area = csisHelpers.resourceInfo.spatial_extent;
-                    //minx = csisHelpers.resourceInfo.minx;
-                    //miny = csisHelpers.resourceInfo.miny;
-                    //maxx = csisHelpers.resourceInfo.maxx;
-                    //maxy = csisHelpers.resourceInfo.maxy;
-                    write_permissions = csisHelpers.resourceInfo.write_permissions;
-                }
-                else if (undefined !== csisHelpers.datapackageInfo && mapType == 'DataPackagePreviewMap' ) {
-                    console.info(`showing ${mapType} for datapackage ${csisHelpers.datapackageInfo.name}`);
-                    datapackage_uuid = csisHelpers.datapackageInfo.uuid;
-                    study_area = csisHelpers.datapackageInfo.spatial_extent;
-                    write_permissions = csisHelpers.datapackageInfo.write_permissions;
-                } else if (undefined !== csisHelpers.studyInfo) { // implicitly use for study preview map
-                    console.info(`showing ${mapType} for study ${csisHelpers.studyInfo.name}`);
-                    study_uuid = csisHelpers.studyInfo.uuid;
-                    study_area = csisHelpers.studyInfo.study_area;
-                    emikat_id = csisHelpers.studyInfo.study_emikat_id;
-                    datapackage_uuid = csisHelpers.studyInfo.study_datapackage_uuid;
-                    write_permissions = csisHelpers.studyInfo.write_permissions;
-                } else if (undefined !== csisHelpers.entityinfo) {
-                    console.warn(`showing ${mapType} for study ${csisHelpers.entityinfo.study_uuid} for **deprecated** drupalSettings.csisHelpers.entityinfo`);
-                    study_uuid = csisHelpers.entityinfo.study_uuid;
-                    study_area = csisHelpers.entityinfo.study_area;
-                    emikat_id = csisHelpers.entityinfo.study_emikat_id;
-                    datapackage_uuid = csisHelpers.entityinfo.study_datapackage_uuid;
-                    write_permissions = csisHelpers.entityinfo.write_permissions;
-                } else {
-                    console.error(`no entityinfo objects found for ${mapType} map component embedded for unsupported entity type!`);
-                }
+            // grouping tag/criteria is defined in custom map components so in principle this renders
+            // those custom map components useless ...
+            mapComponentUrl += grouping_tag ? `&grouping_tag=${grouping_tag}` : '';
 
-                /**
-                 * Base map component URL
-                 * 
-                 * @type {String}
-                 */
-                var mapComponentUrl = `${host}/apps/map-component/build/${mapType}/?host=${host}`;
+            console.debug(`initilizing iFrame with ${mapComponentUrl}`);
+            iFrameMapComponent.setAttribute('src', mapComponentUrl);
 
-                mapComponentUrl += study_uuid ? `&study_uuid=${study_uuid}` : '';
-                mapComponentUrl += study_area ? `&study_area=${study_area}` : '';
-                mapComponentUrl += emikat_id ? `&emikat_id=${emikat_id}` : '';
-                mapComponentUrl += datapackage_uuid ? `&datapackage_uuid=${datapackage_uuid}` : '';
-                mapComponentUrl += write_permissions ? `&write_permissions=${write_permissions}` : '';
-                mapComponentUrl += resource_uuid ? `&resource_uuid=${resource_uuid}` : '';
-                mapComponentUrl += minx ? `&minx=${minx}` : '';
-                mapComponentUrl += miny ? `&miny=${miny}` : '';
-                mapComponentUrl += maxx ? `&maxx=${maxx}` : '';
-                mapComponentUrl += maxy ? `&maxy=${maxy}` : '';
-                // grouping tag/criteria is defined in custom map components so in principle this renders
-                // those custom map components useless ...
-                mapComponentUrl += grouping_tag ? `&grouping_tag=${grouping_tag}` : '';
-
-                console.debug(`initilizing iFrame with ${mapComponentUrl}`);
-                iFrameMapComponent.setAttribute('src', mapComponentUrl);
-            } else {
-                console.error('no global csisHelpers object found, probably not connected to Drupal!')
-            }
         }
         catch (undefinedError) {
             console.error('no global drupalSettings object found, probably not connected to Drupal!', undefinedError);
@@ -106,9 +129,33 @@
     }
 
     /**
-     * TODO
+     * Update iFrame src attribute for Table Component Apps.
+     * 
+     * @param {String} tableType 
+     * @param {Object} iFrameTableComponent 
      */
-    function initTableComponent() {
+    drupalSettings.csisHelpers.initTableComponent = function initTableComponent(tableType = 'GenericTable', iFrameTableComponent = document.getElementById('table-component')) {
+        try {
 
+            if (undefined == iFrameTableComponent || null == iFrameTableComponent) {
+                console.warn('initTableComponent(): no iFrameTableComponent available');
+                return;
+            }
+
+            /**
+             * Base map component URL
+             * 
+             * @type {String}
+             */
+            var tableComponentUrl = `${host}/apps/simple-table-component/build/${tableType}/?host=${host}`;
+
+            tableComponentUrl = addQueryParameters(tableComponentUrl, tableType);
+
+            console.debug(`initilizing iFrame with ${tableComponentUrl}`);
+            iFrameTableComponent.setAttribute('src', tableComponentUrl);
+        }
+        catch (undefinedError) {
+            console.error('no global drupalSettings object found, probably not connected to Drupal!', undefinedError);
+        }
     }
 })(drupalSettings);
