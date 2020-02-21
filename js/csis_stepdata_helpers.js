6d39cb2494be2919f1a18f6fdc8f64e2f85e7145
Patrick Kaleta <patrick.kaleta@ait.ac.at>
2020-02-21 15:52:53 +0100

    added scrolling function to Data tab for resources in DP

--- js/csis_stepdata_helpers.js
+++ js/csis_stepdata_helpers.js
@@ -15,23 +15,35 @@
           if (typeof node !== typeof undefined && node !== false) {
             var id = $(this).attr('data-node-preview');
             var ajaxurl = '/rest/html/resource/summary/' + id +'?_format=json'
-           
-          } 
+
+          }
         }
-        
+
         if (id){
           $.getJSON(ajaxurl, function(result){
             $('.item-preview').html(""); //(result[0].nid
             $.each( result[0], function( key, value ) {
                 $('.item-preview').append('<div class="' + key + '" >' + value + '</div>');
-                
+
             });
+            $('.item-preview').append('<button class="scroll-back">Scroll back up</button>');
+
+            $('html, body').animate({
+              scrollTop: $(".item-preview").offset().top
+            }, 1000);
+
             $('.show-in-preview').removeClass('active');
             $(event.target).addClass('active');
             Drupal.attachBehaviors();
           });
         }
         event.stopImmediatePropagation();
+      });
+
+      $('.scroll-back').on('click', function (event) {
+        $('html, body').animate({
+          scrollTop: $(".show-in-preview.active").offset().top
+        }, 1000);
       });
     }
   };
